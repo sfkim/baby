72876656845881b72c57471d6d4102576914eadb
Soofeel Kim <sf.kim@samsung.com>
2020-01-06 17:54:43 +0900

    [Client] make header & footer components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,65 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/pic.jpeg" width=300>
-    <HelloWorld msg="Hello BUGEON!"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import components
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    //사용할 components 이름 추가(import 이름과 동일)
+    Header, Footer
   }
 }
 </script>
 
+<template>
+  <div id="app">
+    <!-- Header를 위치시킬 공간 지정(top class로 지정)-->
+    <div class='top'>
+      <Header></Header>
+    </div>
+
+    <!-- Footer를 위치시킬 공간 지정(bottom class로 지정)-->
+    <div class='bottom'>
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+
 <style>
+/* for using height 100% */
+html {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  height: 100%;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  overflow: hidden;
+}
+
+/* place top and set area */
+.top {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
+}
+
+/* place bottom and set area */
+.bottom {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
 }
 </style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  //set name 'Header'
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<template>
+  <div class="header-container">
+  </div>
+</template>
+
+<style>
+.header-container {
+  width: 100%;
+  height: 100%;
+  background-color: red;
+}
+</style>
