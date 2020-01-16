b4acb0f3b20b6230a262ae85ef6c54b4a5f257b2
Soofeel Kim <sf.kim@samsung.com>
2020-01-16 16:38:11 +0900

    [Client] revert original css and elements

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,65 +1,34 @@
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'Header',
   props: {
     msg: String
+  },
+  computed: {
+    ...mapGetters([
+      'currentPage'
+    ])
   }
 }
 </script>
 
 <template>
   <div class="header-container">
-    <div class="title">{{msg}}
-      <h2>BB</h2>
-      <nav>
-        <ul>
-          <li><a href="">Introduce</a></li>
-          <li><a href="http://www.naver.com">login</a></li>
-        </ul>
-      </nav>
-    </div>
+    <div class="title">{{currentPage}}</div>
   </div>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Marmelad&display=swap');
-
-*{
- margin: 0px;
- padding: 0px;
-}
 .header-container {
   width: 100%;
   height: 100%;
   background-color: rgb(224, 224, 224);
-  align-items: center;
-  justify-content: space-between;
 }
-
-.header-container ul{
-  font-size: 13px;
-  margin-right: 10px;
-  list-style: none;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.header-container title {
+.title {
   font-size: 25px;
+  padding-top: 16px;
   padding-left: 70px;
-  font-family: 'Marmelad';
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-nav {
-  width: 30%;
-}
-
-a {
-  text-decoration: none;
-  outline: none;
 }
 </style>
