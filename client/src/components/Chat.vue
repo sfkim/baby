ab5318d208ea66196c8b3c66f2509214a05fc925
Soofeel Kim <rlatnvlf88@gmail.com>
2020-01-27 22:47:54 +0900

    [Client] make fake data in chat.vue

--- client/src/components/Chat.vue
+++ client/src/components/Chat.vue
@@ -11,7 +11,8 @@ export default {
   },
   data () {
     return {
-      userMsg: '# H1 string  \n  ## h2 Heading  \n  ### h3 Heading  \n  ## Horizontal Rules \n # H1 string  \n  ## h2 Heading  \n  ### h3 Heading  \n  ## Horizontal Rules \n # H1 string  \n  ## h2 Heading  \n  ### h3 Heading  \n  ## Horizontal Rules \n'
+      userMsg: '# H1 string  \n  ## h2 Heading  \n  ### h3 Heading  \n  ## Horizontal Rules \n # H1 string  \n  ## h2 Heading  \n  ### h3 Heading  \n  ## Horizontal Rules \n # H1 string  \n  ## h2 Heading  \n  ### h3 Heading  \n  ## Horizontal Rules \n',
+      userTable: '## 신체 성장 표준치 \n ### 남아 1개월 \n  |백분위수|몸무게(kg)|키(cm)|머리둘레(cm)| \n |:-:|:-:|:-:|:-:| \n 3|3.4|51.1|35.1| \n  5|3.6|51.5|35.4| \n 10|3.8|52.2|35.8| \n 15|3.9|52.7|36.1| \n 25|4.1|53.4|36.5| \n <span style="background-color:rgb(200, 237, 230)">50</span>|<span style="background-color:rgb(200, 237, 230)">4.5</span>|<span style="background-color:rgb(200, 237, 230)">54.7</span>|<span style="background-color:rgb(200, 237, 230)">37.3</span>| \n 75|4.9|56.0|38.1| \n 85|5.1|56.7|38.5| \n 90|5.3|57.2|38.8| \n 95|5.5|57.9|39.2| \n 97|5.7|58.4|39.5| \n'
     }
   },
   computed: {
@@ -33,7 +34,7 @@ export default {
     <div class='message-container'>
         <div class='popo-profile'></div>
         <div class='md-container'>
-            <vue-markdown>{{userMsg}}</vue-markdown>
+            <vue-markdown>{{userTable}}</vue-markdown>
         </div>
     </div>
     <div class='message-container'>
@@ -77,11 +78,12 @@ export default {
 }
 
 .md-container {
-    width: 320px;
+    width: 310px;
     min-height: 50px;
     max-height: 500px;
     margin-left: 75px;
     margin-bottom: 15px;
+    padding-left: 10px;
     overflow: scroll;
     background-color: rgb(224, 224, 224);
     border-radius: 3px 13px 13px 13px !important;
@@ -104,8 +106,8 @@ export default {
 
 .md-container-right {
     width: 200px;
-    min-height: 50px;
-    max-height: 500px;
+    height: 36px;
+    max-height: 100px;
     word-break: break-word;
     text-align: right;
     padding: 8px !important;
