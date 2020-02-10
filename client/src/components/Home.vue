50732c24512ba785147572075e448cb3e2cddfa7
bugeonkim <wittybear@naver.com>
2020-02-10 22:10:54 +0900

    [Client] Added Menu.vue in Home.vue

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,28 +1,32 @@
 <script>
 import Search from './Search.vue'
+import Menu from './Menu.vue'
 
 export default {
   name: 'Home',
   components: {
-    Search
+    Search, Menu
   }
 }
 </script>
 
 <template>
-  <div>Home page
-    <div class="embed-container" style="TEXT-ALIGN: center">
-      <object type="text/html" width="320" height="180" data="//www.youtube.com/v/dEPirXVVhhs?autoplay=1&loop=1" allowscreen=""/>
-    </div>
-    <div class="search-contaginer">
+  <div>
+    <div class="search-container">
       <Search></Search>
+    </div>
+    <div class="menu-container">
+      <Menu></Menu>
     </div>
   </div>
 
 </template>
 
 <style>
-.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; height: auto; }
-.embed-container object,
-.embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
+.search-container {
+    width: 100%;
+    height: 50px;
+    padding-top: 10px;
+    padding-bottom 10px;
+}
 </style>
