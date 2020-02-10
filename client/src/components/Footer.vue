f3bec3b22a0eade3d65d1f6f4172dba1c6f08f76
bugeonkim <wittybear@naver.com>
2020-02-10 21:51:56 +0900

    Merge branch 'master' of /Users/bugeonkim/PycharmProjects/baby with conflicts.

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -36,11 +36,25 @@ import { mapGetters } from 'vuex';
 
 <template>
   <div class="footer-container">
-    <div id="icon-home" class="icon-item" @click="onClickHome"></div>
-    <div id="icon-my-info" class="icon-item" @click="onClickMyInfo"></div>
-    <div id="icon-chat" class="icon-item" @click="onClickChat"></div>
-    <div id="icon-wiki" class="icon-item" @click="onClickWiki"></div>
-    <div id="icon-setting" class="icon-item" @click="onClickSetting"></div>
+    <div id="icon-home" class="icon-item" @click="onClickHome">
+      <div class="icon-title">홈</div>
+    </div>
+    <div id="icon-my-info" class="icon-item" @click="onClickMyInfo">
+      <div class="icon-title">나의 아기</div>
+    </div>
+
+    <div id="icon-chat" class="icon-item" @click="onClickChat">
+      <div class="icon-title">채팅</div>
+    </div>
+
+    <div id="icon-wiki" class="icon-item" @click="onClickWiki">
+      <div class="icon-title">아기 사전</div>
+    </div>
+
+    <div id="icon-setting" class="icon-item" @click="onClickSetting">
+      <div class="icon-title">설정</div>
+    </div>
+
   </div>
 </template>
 
@@ -48,16 +62,27 @@ import { mapGetters } from 'vuex';
 .footer-container {
   width: 100%;
   height: 100%;
-  background-color: rgb(200, 237, 230);
 }
 
 .icon-item {
   width: 20%;
   height: 100%;
-  background-size: 60px;
+  background-size: 40px;
   background-repeat: no-repeat;
-  background-position: center center;
+  background-position: center top;
+  background-color: rgb(200, 237, 230);
   float: left;
+}
+
+.icon-item:hover {
+  filter: brightness(90%);
+}
+
+.icon-title {
+  position: absolute;
+  bottom: 0;
+  width: 20%;
+  text-align: center;
 }
 
 #icon-home {
--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -10,7 +10,10 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div>Home page
+    <div class="embed-container" style="TEXT-ALIGN: center">
+      <object type="text/html" width="320" height="180" data="//www.youtube.com/v/dEPirXVVhhs?autoplay=1&loop=1" allowscreen=""/>
+    </div>
     <div class="search-container">
       <Search></Search>
     </div>
@@ -19,9 +22,7 @@ export default {
 </template>
 
 <style>
-.search-container {
-    width: 100%;
-    height: 50px;
-    padding-top: 10px;
-}
+.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; height: auto; }
+.embed-container object,
+.embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
 </style>
