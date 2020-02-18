2cb6c737db50bd5f7aa12d4fdadd96f01ffd77a6
Soofeel Kim <sf.kim@samsung.com>
2020-02-18 14:09:00 +0900

    [DM] fix bug, content.json & config.py

--- DM/config.py
+++ DM/config.py
@@ -1,4 +1,4 @@
 ### Server Setting ###
 SERVER_PORT = 8895
 SERVER_DEBUG_MODE = True
-CLASSIFY_SERVER_URL = "http://10.165.90.78:8885/get_prediction"
+CLASSIFY_SERVER_URL = "http://15.165.90.78:8885/get_prediction"
