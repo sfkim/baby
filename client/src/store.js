4f8a51163e89dd62b98fbb2f427136aceed194b5
Soofeel Kim <sf.kim@samsung.com>
2020-04-13 12:29:19 +0900

    [Client] get information from 'babyInformation'  - set/get babyinformation from localstorage  - add package to package.json

--- client/src/store.js
+++ client/src/store.js
@@ -49,7 +49,9 @@ function getMonthContents (state, currentMonth) {
   // 신체 성장 표준치
   const growthID = 240;
   let growthChip = currentMonth;
-  if (state.babyInformation[0].babySex == 0) growthChip += 100;
+
+  if (state.babyInformation == null) growthChip += 100;
+  else if (state.babyInformation[0].babySex == 0) growthChip += 100;
 
   Axios.get(DM_SERVER + 'get_content?classID=' + growthID + '&chip=' + growthChip)
     .then(res => {
@@ -82,8 +84,6 @@ function getMonthContents (state, currentMonth) {
             })
         })
     })
-
-  console.log(state.monthInformation);
 }
 
 const store = new Vuex.Store ({
